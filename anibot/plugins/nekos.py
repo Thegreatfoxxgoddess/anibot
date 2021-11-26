50729a7d47de7190dee37ebdaf49fedfba02b9d9
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-11-27 02:48:43 +0530

    Update nekos.py

--- anibot/plugins/nekos.py
+++ anibot/plugins/nekos.py
@@ -18,7 +18,7 @@ for m in SFW:
     neko_help += f"<code>{m.lower()}</code>   "
 
 
-@anibot.on_message(filters.command(["nekos", prefixes=trg))
+@anibot.on_message(filters.command(("nekos", prefixes=trg))
 @control_user
 async def neko_life(message: Message):
     choice = message.input_str
